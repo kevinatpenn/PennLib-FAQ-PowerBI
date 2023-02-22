29a0467b7d9a4f6c9ea92ff839069249646f16dc
kevinatpenn <44673990+kevinatpenn@users.noreply.github.com>
2023-02-22 12:25:31 -0500

    Update cleaner_master.R
    Update filenames during live run of data update

--- application/cleaner_master.R
+++ application/cleaner_master.R
@@ -2,16 +2,16 @@
 
 setwd("C:/Users/kevinat/Documents/GitHub/PennLib-FAQ-PowerBI/data")
 
-allfaqviews_old <- "la_faq_views_clean.csv"
-allfaqviews_new <- "allfaqviews_2022_12_29.csv" # Update this filename
+allfaqviews_old <- "allfaqviews_clean.csv"
+allfaqviews_new <- "allfaqviews_2023_2_22.csv" # Update this filename
 allfaqviews_out <- allfaqviews_old
 
 faq_export_old <- "la_faq_export_clean.csv"
-faq_export_new <- "la_faq_export2022-12-28_04_20_18.csv" # Update this filename
+faq_export_new <- "la_faq_export2023-02-22_12_17_34.csv" # Update this filename
 faq_export_out <- faq_export_old
 
 queryanalyzer_old <- "la_queryanalyzer_store.csv"
-queryanalyzer_new <- "queryanalyzer_707_2022-12-29_11_25_06.csv" # Update this filename
+queryanalyzer_new <- "queryanalyzer_707_2023-02-22_12_17_49.csv" # Update this filename
 queryanalyzer_out <- "la_queryanalyzer_clean.csv"
 
 
