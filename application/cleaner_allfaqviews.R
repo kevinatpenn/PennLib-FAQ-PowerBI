61f36b7e69462fef3258835fcb1339a26db99005
Kevin A Thomas <44673990+kevinatpenn@users.noreply.github.com>
2023-08-01 11:25:18 -0400

    Update cleaner_allfaqviews.R
    Adjusted for change in LibAnswers column order

--- application/cleaner_allfaqviews.R
+++ application/cleaner_allfaqviews.R
@@ -7,7 +7,7 @@ if( !("tidyr" %in% installed.packages()) ){
 
 # Load data
 
-#setwd("C:/Users/kevinat/Documents/PennLib-FAQ-PowerBI/data")
+#setwd("~/Documents/GitHub/PennLib-FAQ-PowerBI/data")
 
 names_old <- read.csv(allfaqviews_old,
                     header = FALSE,
@@ -26,14 +26,22 @@ names(dat_new)[1:2] <- names_old[1:2]
 
 dat_new <- tidyr::pivot_longer(dat_new,
                                cols = -1:-2,
-                               names_to = "Year.Month",
+                               names_to = "Month.Year",
                                names_prefix = "X",
                                values_to = names_old[ , ncol(names_old)])
 dat_new <- dat_new[dat_new[ , ncol(dat_new)] != 0, ]
-dat_new <- tidyr::separate(dat_new,#[ , "Year.Month"],
-                           col = "Year.Month",
-                           into = unlist(names_old[ , 3:4]),
+dat_new <- tidyr::separate(dat_new,#[ , "Month.Year"],
+                           col = "Month.Year",
+                           into = c("Month", "Year"),
                            sep = "\\.")
+
+
+# Rearrange columns to match Power BI expected order
+
+dat_new <- data.frame(dat_new[ , 1:2],
+                      dat_new[ , 4],
+                      dat_new[ , 3],
+                      dat_new[ , 5])
 
 
 # Export new data
