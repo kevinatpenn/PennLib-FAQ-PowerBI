45e4b5c3537a6b84cecda45668dbdaaf11ab11c7
Kevin A Thomas <44673990+kevinatpenn@users.noreply.github.com>
2023-08-01 11:58:28 -0400

    Update cleaner_allfaqviews.R
    Made column names static to simplify corrections for column changes

--- application/cleaner_allfaqviews.R
+++ application/cleaner_allfaqviews.R
@@ -9,17 +9,8 @@ if( !("tidyr" %in% installed.packages()) ){
 
 #setwd("~/Documents/GitHub/PennLib-FAQ-PowerBI/data")
 
-names_old <- read.csv(allfaqviews_old,
-                    header = FALSE,
-                    nrows = 1)
 dat_new <- read.csv(allfaqviews_new, # Update this filename
                     skip = 3)
-
-
-# Restore initial column names
-
-names_old[1, 1] <- "FAQID"
-names(dat_new)[1:2] <- names_old[1:2]
 
 
 # Lengthen data
@@ -28,7 +19,7 @@ dat_new <- tidyr::pivot_longer(dat_new,
                                cols = -1:-2,
                                names_to = "Month.Year",
                                names_prefix = "X",
-                               values_to = names_old[ , ncol(names_old)])
+                               values_to = "Public Views")
 dat_new <- dat_new[dat_new[ , ncol(dat_new)] != 0, ]
 dat_new <- tidyr::separate(dat_new,#[ , "Month.Year"],
                            col = "Month.Year",
@@ -39,9 +30,13 @@ dat_new <- tidyr::separate(dat_new,#[ , "Month.Year"],
 # Rearrange columns to match Power BI expected order
 
 dat_new <- data.frame(dat_new[ , 1:2],
-                      dat_new[ , 4],
-                      dat_new[ , 3],
+                      dat_new[ , 4], # Year
+                      dat_new[ , 3], # Month
                       dat_new[ , 5])
+
+
+# Restore historical column names
+names(dat_new) <- c("FAQID", "Question", "Year", "Month", "Public Views")
 
 
 # Export new data
